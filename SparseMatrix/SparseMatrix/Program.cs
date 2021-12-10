d558e1f5941e4f19fcd27eb8a180e741ea367786
Alson <filsalouna@gmail.com>
2021-12-10 11:36:25 +0300

    First commit

---
+++ SparseMatrix/SparseMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SparseMatrix
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               SparseMatrix<int> sm = new SparseMatrix<int>(4, 4);
+               sm[1, 1] = 2;
+               sm[2, 1] = 3;
+               sm[0, 1] = 1;
+               List<int> lst = sm.Neighbour(3, 0);
+               foreach (var e in lst)
+               {
+                    Console.Write($"{e} ");
+               }
+               Console.WriteLine();
+               foreach (var e in sm)
+               {
+                    Console.Write($"{e} ");
+               }
+               
+          }
+     }
+}
---
+++ SparseMatrix/SparseMatrix/SparseMatrix.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SparseMatrix
+{
+     public class SparseMatrix<T>:IEnumerable<T>
+     {
+          private Dictionary<(int, int), T> Elem = new Dictionary<(int, int), T>();
+
+          private int n;
+          private int m;
+
+          public int N => n;
+          public int M => m;
+          public SparseMatrix(int n, int m)
+          {
+               this.n = n;
+               this.m = m;
+          }
+
+          public SparseMatrix(int n, int m, Dictionary<(int, int), T> dictionary)
+          {
+               this.n = n;
+               this.m = m;
+               Elem   = dictionary;
+          }
+
+          public T this[int i, int j]
+          {
+               get
+               {
+                    if (Elem.ContainsKey((((i%n)+n)%n, ((j%m)+m)%m)))
+                         return Elem[(((i %n) +n) %n, ((j %m) +m) %m)];
+                    return default;
+               }
+               set
+               {
+                    if (Elem.ContainsKey((((i %n) +n) %n, ((j %m) +m) %m)))
+                         Elem[(((i %n) +n) %n, ((j %m) +m) %m)] = value;
+                    else if (!value.Equals(default(T)))
+                         Elem.Add((((i %n) +n) %n, ((j %m) +m) %m), value);
+               }
+          }
+
+          public List<T> Neighbour(int row, int col)
+          {
+               List<T> lst = new List<T>();
+               for (int i = -1; i <= 1; ++i)
+               for (int j = -1; j <= 1; ++j)
+               {
+                    if (i == 0 && j == 0)
+                         continue;
+                    lst.Add(this[row + i, col + j]);
+               }
+
+               return lst;
+          }
+
+
+          public IEnumerator<T> GetEnumerator()
+          {
+               for (int i = 0; i < n; ++i)
+               for (int j = 0; j < m; ++j)
+                    yield return this[i, j];
+          }
+          
+
+          IEnumerator IEnumerable.GetEnumerator()
+          {
+               return GetEnumerator();
+          }
+     }
+}
